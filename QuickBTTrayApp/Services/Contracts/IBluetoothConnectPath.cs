969dc8fae87e607bc537b58856c9257803623c15
Peter Tissen <dravorek@gmail.com>
2026-09-05 22:12:34 +0200

    move c_port files into root and delete C# files

--- QuickBTTrayApp/Services/Contracts/IBluetoothConnectPath.cs
+++
@@ -1,8 +0,0 @@
-namespace QuickBTTrayApp.Services.Contracts
-{
-    /// <summary>Connects one BT device. Separate interface so API or UI path can be removed independently.</summary>
-    public interface IBluetoothConnectPath
-    {
-        Task<DeviceToggleResult> ConnectAsync(string deviceName, string deviceAddress);
-    }
-}
